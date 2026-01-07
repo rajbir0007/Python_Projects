d713a73a7108b52be79343274616a0ec057ef94c
Rajbir Singh <rajbir199925@gmail.com>
2026-01-07 21:38:37 +0530

    new_code

---
+++ Bidder.py
@@ -0,0 +1,37 @@
+from os import system
+
+def main():
+    print(f"{'*'*40}'Welcome to the Bid'{'*'*40}")
+
+    flag = 'yes'
+    bidders={}
+    while flag =='yes':
+        name=input('Enter you name: ')
+        bid=input('Enter the bid amount: ')
+        while not bid.isdigit():
+            print('Please enter bid amount as numeric')
+            bid=input('Enter the bid amount: ')
+        bidders[name]=int(bid)
+
+        flag=input('Do you want to continue the bid ?:(Enter "yes" or "no" only!) ').lower()
+        while flag not in ['yes','no']:
+            flag=input('Kindly Enter "yes" or "no" only!').lower()
+        if flag=='yes':
+            system("clear")
+
+    high=0
+    winner=''
+    for key,value in bidders.items():
+        if value>high:
+            high=value
+            winner=key
+    print(f'Congratulations {winner} you have won the bid !!!!')
+
+
+
+        
+            
+if __name__=='__main__':
+    main()
+
+    
